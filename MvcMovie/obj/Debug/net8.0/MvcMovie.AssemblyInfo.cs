40da1036a68bad884d518648d89a69641ddf07b7
nhuynh5 <nhuynh5@huskers.unl.edu>
2024-04-12 12:11:33 -0500

    Added Tests folder

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f0928d548570c0c6b3f3a118497355ab1562e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec18c251be9574f00ada0274f2ba404a7918677")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
