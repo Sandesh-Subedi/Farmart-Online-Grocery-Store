2ce8cde7210103d3269936535443c18d85d2685d
Jakeerwin <111807436+Jakeerwin@users.noreply.github.com>
2024-04-17 11:52:53 -0500

    Added 'About Us' Page

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2eda9d07163ac51187b4fa07ff0ec3b677addc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4301ba81746f996c98545f1cbd00afa0100f26cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
