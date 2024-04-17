96f3e714599df7e96a899808aa9659d21d74d289
Kyle Gann <kgann2@huskers.unl.edu>
2024-04-17 12:14:40 -0500

    AddedSignInTester

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d401658f3b059e26dd4459c0f19a71cc1a29cb80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fa3d22a1a79fac510eb588b481aa9243aec8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
