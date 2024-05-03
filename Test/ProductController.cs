e14674f357cf71403a01142d8e0c660f80f98866
Yusup Orazov <152412703+Yusup04Orazov@users.noreply.github.com>
2024-05-03 11:00:12 -0500

    Update ProductController.cs

--- Test/ProductController.cs
+++ Test/ProductController.cs
@@ -1,1 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyOnlineStore.Models;
+using System.Linq;
 
+namespace MyOnlineStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(string search)
+        {
+            var query = _context.Items.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search))
+            {
+                query = query.Where(item =>
+                    item.Name.Contains(search) ||
+                    item.Description.Contains(search));
+            }
+
+            var filteredItems = query.ToList();
+            return View(filteredItems);
+        }
+    }
+}
