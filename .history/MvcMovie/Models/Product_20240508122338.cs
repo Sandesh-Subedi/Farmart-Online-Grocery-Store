0b4068a792761fd5a97b5ccd8287c8c1d47af2fa
jakeerwin <jakeerwin7@outlook.com>
2024-05-08 17:36:07 -0500

    Added Search Bar

---
+++ .history/MvcMovie/Models/Product_20240508122338.cs
@@ -0,0 +1,23 @@
+﻿namespace MvcMovie.Models
+{
+    public class Product(
+        int productId,
+        string name,
+        string description,
+        string category,
+        double weight,
+        string dateGrown,
+        string dateHarvested,
+        string imageURL)
+    {
+        public int ProductId { get; set; } = productId;
+        public string Name { get; set; } = name;
+        public string Description { get; set; } = description;
+        public string Category { get; set; } = category;
+        public double Weight { get; set; } = weight;
+        public string DateGrown { get; set; } = dateGrown;
+        public string DateHarvested { get; set; } = dateHarvested;
+        public string imageURL { get; set; } = imageURL;
+
+    }
+}
